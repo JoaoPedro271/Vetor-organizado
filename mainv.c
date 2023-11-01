e0029e6520bce19981bcf13ce937a4160cc10cc6
Joao Pedro Soares Almeida <joao.almeida325@etec.sp.gov.br>
2023-11-01 17:37:12 -0300

    Criado

---
+++ mainv.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+
+	int n1;
+	int list;
+
+
+	for(list=0; list < 8; list=list + n1){
+
+		n1 = n1 > n1;
+
+		printf("Digite os valores ");
+		scanf("%d",&n1);
+
+		printf("Voce adicionou: %d ",list);
+
+	}
+
+
+
+}
